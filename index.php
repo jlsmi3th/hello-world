47755ee02b0a457d9c490ad39d06b7ced4cea4f1
James L Smith <jlsmi3th@gmail.com>
2019-11-17 12:25:43 -0700

    my first commit

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php require_once "./header.inc.php"; ?>
+<title>San Juan High School class of 1965 Index</title>
+<?php require_once "./header-cont.inc.php"; ?>
+
+<?php require_once "./page-header.inc.php"; ?>
+<?php require_once "./nav-top.inc.php"; ?>
+
+<!--
+      <img src="images/spartan4.gif"
+           border="0"
+           alt="Spartan logo">
+
+      <p class="notice">
+        Notice of big change!
+        <br>
+        The price has been dropped to just $50 per person.
+        Act now to have a wonderful time swapping stories
+        with your classmates!
+      </p>
+      <p>
+        The Radisson
+        at
+        500 Leisure Lane Sacramento, CA 95815
+        is our venue for this year's reunion
+        on September 25 and 26.
+        For complete information see the "Reunion Info" page,
+        link to the left.
+        As you can see,
+        our website now stands alone
+        and no longer has to lurk within my personal website.
+        This may make it easier for the various search engines to find
+        and, therefore, may improve our chances of classmates
+        finding the site and attending the event.
+      </p>
+-->
+<!--
+      <p><b>Flash! Hot off the presses!</b></p>
+      <p>
+        A new FaceBook group,
+        <br>
+        <b>San Juan High School Class of 1965, Citrus Heights CA</b>
+        <br>
+        has been created as a closed group
+        for our classmates and friends of the class of '65.
+        This means the group is visible to all
+        but only members may make posts.
+        I started with my personal list of FaceBook friends as the starter nucleus
+        of the membership roster.
+        Any member may add new members so I hope this FB group will grow rapidly
+        and become a lively adjunct to this website.
+      </p>
+      <hr>
+-->
+<h2>
+  It doesn’t seem possible that it’s almost time for our 55th Reunion!
+</h2>
+<p>
+  This reunion is shaping up
+  to be an evening event,
+  to distinguish it from our annual <i>lunch</i> reunions,
+  tentatively be held
+  sometime in September 2020.
+</p>
+<p>
+  We’re looking into having it somewhere in Folsom.
+  Maybe Natomas Inn?
+  It’s a great location
+  and walking distance to so many things to do
+  in Old Town Folsom.
+  Those who stayed at the recommended hotel last time said
+  the next morning breakfast provided
+  another great opportunity to socialize.
+</p>
+<p>
+  As for the event,
+  we’re thinking of a casual evening with hors d'oeuvres
+  that would encourage people to be more mobile for visiting
+  than just sitting at tables and being served.
+  We’re also considering having a DJ
+  for dancing or background entertainment.
+</p>
+<p>
+    At this point we have not decided
+    on a price or single contact person.
+    Your questions and comments may be directed
+    to those noted on the
+    <a href="contact.php">"Contact Us"</a> page.
+</p>
+<p>
+  Please to spread the word
+  to your Spartan friends!
+</p>
+<hr>
+      <p>
+        Please contact those classmates with whom you have kept in touch
+        to bring this website to their attention and
+        to give us their latest contact information
+        so we can apprise them of future events.
+      </p>
+      <p>
+        This information only goes into the class database
+        and is not in any way shared with
+        or sold to any other entity.
+      </p>
+      <p>
+        <b>Note:</b> the members of the current reunion committee
+        and how to contact them can be found on the
+       <a href="contact.php"><b>Contact Us page.</b></a>
+      </p>
+ 
+<?php require_once "./footer.inc.php"; ?>
